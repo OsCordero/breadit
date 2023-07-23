0f97260b052cb80be6531cd42184d4daaf3fe687
Oscar <oscarcordero97@gmail.com>
2023-07-23 03:55:27 -0600

    post styles fix

--- src/components/renderers/ImageRenderer.tsx
+++ src/components/renderers/ImageRenderer.tsx
@@ -4,7 +4,7 @@ function ImageRenderer({ data }: any) {
   const src = data.file.url;
 
   return (
-    <div className="relative w-full min-h-[15rem]">
+    <div className="relative w-full min-h-[15rem] not-prose mb-4">
       <Image alt="image" className="object-contain" fill src={src} />
     </div>
   );
